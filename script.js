ea4aec93cd0acc4f86a84fc1eb6f7eba590592c9
Pedro Henrique <128756451+ph-ol@users.noreply.github.com>
2023-05-14 23:29:45 -0300

    caseSucesso.html

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    var elementBody = document.querySelector('body');
+    var elementBtnIncreaseFont = document.getElementById('increase-font');
+    var elementBtnDecreaseFont = document.getElementById('decrease-font');
+    var fontSize = 100;
+    var increaseDecrease = 22;
+
+    elementBtnIncreaseFont.addEventListener('click', function(event) {
+        fontSize = fontSize + increaseDecrease;
+        elementBody.style.fontSize = fontSize + '%';
+    });
+    
+    elementBtnDecreaseFont.addEventListener('click', function(event) {
+        fontSize = fontSize - increaseDecrease;
+        elementBody.style.fontSize = fontSize + '%';
+    });
+}
